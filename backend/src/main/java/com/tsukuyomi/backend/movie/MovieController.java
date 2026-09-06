987b0cf0cd33e4e5eee2ea0710eddeedd34aaa71
Victor <victorfariasbezerra12@gmail.com>
2026-09-06 15:11:06 -0300

    Movie Controller

---
+++ backend/src/main/java/com/tsukuyomi/backend/movie/MovieController.java
@@ -0,0 +1,23 @@
+package com.tsukuyomi.backend.movie;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private final MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+}
