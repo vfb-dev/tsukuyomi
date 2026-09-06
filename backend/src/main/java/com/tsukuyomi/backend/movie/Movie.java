2cbd4edbfa3b68e32ffb70c10a560718a2eef387
Victor <victorfariasbezerra12@gmail.com>
2026-09-05 23:23:06 -0300

    Movie Entity

---
+++ backend/src/main/java/com/tsukuyomi/backend/movie/Movie.java
@@ -0,0 +1,27 @@
+package com.tsukuyomi.backend.movie;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Integer releaseYear;
+    private Integer durationMinutes;
+    private String posterUrl;
+    private String videoUrl;
+}
