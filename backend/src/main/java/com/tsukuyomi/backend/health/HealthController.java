39741988cc524fe6ec617ab92568266dd9edc6c2
Victor <victorfariasbezerra12@gmail.com>
2026-09-05 21:08:49 -0300

    Starting backend

---
+++ backend/src/main/java/com/tsukuyomi/backend/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.tsukuyomi.backend.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Tsukuyomi backend is running";
+    }
+}
